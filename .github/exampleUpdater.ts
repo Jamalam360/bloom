71fec08d5bdf315536fbf646d55e9d09726b9918
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-02-02 17:53:21 +0000

    feat: stop actions failing sometimes maybe

--- .github/exampleUpdater.ts
+++ .github/exampleUpdater.ts
@@ -2,4 +2,23 @@ let template = await Deno.readTextFile("./.github/README_TEMPLATE.md");
 const example = await Deno.readTextFile("./example.ts");
 
 template = template.replace("${EXAMPLE}", example);
-await Deno.writeTextFile("./README.md", template);
+
+if (await Deno.readTextFile("./README.md") != template) {
+  await Deno.writeTextFile("./README.md", template);
+
+  Deno.run({
+    cmd:
+      `git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com`
+        .split(" "),
+  });
+
+  Deno.run({
+    cmd: `git config --local user.name "github-actions"`
+      .split(" "),
+  });
+
+  Deno.run({
+    cmd: `git commit -m "[Bot] Update Example" -a`
+      .split(" "),
+  });
+}
