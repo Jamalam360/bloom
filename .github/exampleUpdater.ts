deaaa234052ee3c228432333bd96177fc96342e2
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-02-02 17:54:42 +0000

    chore: revert that

--- .github/exampleUpdater.ts
+++ .github/exampleUpdater.ts
@@ -2,23 +2,4 @@ let template = await Deno.readTextFile("./.github/README_TEMPLATE.md");
 const example = await Deno.readTextFile("./example.ts");
 
 template = template.replace("${EXAMPLE}", example);
-
-if (await Deno.readTextFile("./README.md") != template) {
-  await Deno.writeTextFile("./README.md", template);
-
-  Deno.run({
-    cmd:
-      `git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com`
-        .split(" "),
-  });
-
-  Deno.run({
-    cmd: `git config --local user.name "github-actions"`
-      .split(" "),
-  });
-
-  Deno.run({
-    cmd: `git commit -m "[Bot] Update Example" -a`
-      .split(" "),
-  });
-}
+await Deno.writeTextFile("./README.md", template);
