dab9dd17da17b19f4c1764ac799b65535fc853f2
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-02-02 17:41:22 +0000

    fix: github actions file paths

--- .github/exampleUpdater.ts
+++ .github/exampleUpdater.ts
@@ -2,4 +2,4 @@ const template = await Deno.readTextFile("./.github/README_TEMPLATE.md");
 const example = await Deno.readTextFile("./example.ts");
 
 template.replace("${EXAMPLE}", example);
-await Deno.writeTextFile("../README.md", template);
+await Deno.writeTextFile("./README.md", template);
