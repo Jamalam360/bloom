f2a97b9f99af26f3b2ac89d5d16838452e4c589f
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-02-02 17:47:38 +0000

    temp: add debug logging to actions

--- .github/exampleUpdater.ts
+++ .github/exampleUpdater.ts
@@ -2,4 +2,5 @@ const template = await Deno.readTextFile("./.github/README_TEMPLATE.md");
 const example = await Deno.readTextFile("./example.ts");
 
 template.replace("${EXAMPLE}", example);
+console.log(template);
 await Deno.writeTextFile("./README.md", template);
