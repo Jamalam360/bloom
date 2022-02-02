dbc6a7763fac7302aba638f198fb3af551118dc7
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-02-02 17:36:56 +0000

    feat: update example more

--- example.ts
+++ example.ts
@@ -1,3 +1,14 @@
-import { BloomFilter } from "https://raw.githubusercontent/Jamalam360/bloom/mod.ts";
+import { BloomFilter } from "https://raw.githubusercontent.com/Jamalam360/bloom/main/mod.ts";
 
-const filter = new BloomFilter();
+// number of elements: number,
+// false positive probability: number (0 - 1)
+// These values are used to calculate the size of
+// the bit array the Bloom filter uses
+const filter = new BloomFilter(10, 0.05);
+
+console.log(filter.toBitString());
+filter.add("in filter");
+console.log(filter.toBitString());
+
+console.log(filter.check("in filter"));
+console.log(filter.check("not in filter"));
