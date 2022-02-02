120be0aa8bbb0e0bf0ddf57ec91da3d52847305d
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-02-02 17:49:34 +0000

    fix: update example puller script

--- .github/exampleUpdater.ts
+++ .github/exampleUpdater.ts
@@ -1,6 +1,5 @@
-const template = await Deno.readTextFile("./.github/README_TEMPLATE.md");
+let template = await Deno.readTextFile("./.github/README_TEMPLATE.md");
 const example = await Deno.readTextFile("./example.ts");
 
-template.replace("${EXAMPLE}", example);
-console.log(template);
+template = template.replace("${EXAMPLE}", example);
 await Deno.writeTextFile("./README.md", template);
